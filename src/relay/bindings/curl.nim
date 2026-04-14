3a9c2ba0279bbf1981512ba49995bb148baacb8d
Antonis Geralis <b3lievem@yandex.com>
2026-04-14 12:07:27 +0300

    cleanup dont use enum

--- src/relay/bindings/curl.nim
+++ src/relay/bindings/curl.nim
@@ -8,14 +8,10 @@ type
   CURLoption* = cint
   CURLMoption* = cint
   CURLINFO* = cint
+  CurlMsgType* = cint
 
   curl_slist* {.importc: "struct curl_slist", header: "<curl/curl.h>",
       incompleteStruct.} = object
-
-  CurlMsgType* = enum
-    CURLMSG_NONE = 0,
-    CURLMSG_DONE = 1,
-    CURLMSG_LAST = 2
 
   CURLMsgData* {.union.} = object
     whatever*: pointer
@@ -80,6 +76,10 @@ const
   CURLPIPE_NOTHING* = clong(0)
   CURLPIPE_HTTP1* = clong(1)
   CURLPIPE_MULTIPLEX* = clong(2)
+
+  CURLMSG_NONE = CurlMsgType(0)
+  CURLMSG_DONE = CurlMsgType(1)
+  CURLMSG_LAST = CurlMsgType(2)
 
   CURLINFO_LONG* = 0x200000
   CURLINFO_STRING* = 0x100000
